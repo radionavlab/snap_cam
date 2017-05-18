bda3ec1f53b134c226f7872cca038e1dbe1f62e6
Greg Anders <ganders@utexas.edu>
2017-05-18 13:46:04 -0500

    Flip frame so image is right side up

--- src/SnapCam.cpp
+++ src/SnapCam.cpp
@@ -256,6 +256,8 @@ void SnapCam::onVideoFrame(ICameraFrame *frame)
 	if (config_.func == 0) { //highres
 		cv::Mat mYUV = cv::Mat(1.5 * frame_height, frame_width, CV_8UC1, frame->data);
 		cv::cvtColor(mYUV, matFrame, CV_YUV420sp2RGB);
+               cv::transpose(matFrame, matFrame);
+               cv::flip(matFrame, matFrame, 1);
 		mYUV.deallocate();
 
 	} else { //optical flow
