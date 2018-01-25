dd1c6cff7c1878d8d23cdd6022ef100223f912b9
tuckerhaydon <tuckerhaydon@gmail.com>
2018-01-25 14:40:31 -0600

    Added comments to callback.cc

--- src/callback.cc
+++ src/callback.cc
@@ -11,14 +11,16 @@ void callback(const cv::Mat& img,
         /* 
         * YOUR CODE GOES HERE
         */
+
+        // Example: Printing info
         std::cout << "Got image! " << img.size[0] << ", " << img.size[1] << std::endl;
         std::cout << "Camera params: " << f << ", " << k1 << std::endl;
 
+        // Example: Printing FPS.
         static long long last_time = 0;
         struct timeval tp;
         gettimeofday(&tp, NULL);
         long long now_time = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-
         std::cout << "FPS: " << 1000 / (now_time - last_time) << std::endl;
         last_time = now_time;
 
