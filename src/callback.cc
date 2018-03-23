833da11b1b11c71d8b042c9e8bee998b585881b1
tuckerhaydon <tuckerhaydon@gmail.com>
2018-03-23 16:24:36 -0500

    Removed transforms. Logging raw data now.

--- src/callback.cc
+++ src/callback.cc
@@ -3,7 +3,6 @@
 #include <sys/time.h>
 #include "sensorParams.h"
 #include "GPS.h"
-#include "transform.h"
 
 #include <sstream>
 #include <opencv2/opencv.hpp>
@@ -27,40 +26,34 @@ void saveImage(const cv::Mat& img, const std::string saveDirectory) {
     std::cout << "FPS: " << 1000.0 / (timeNow - timeLast) << std::endl;
     timeLast = timeNow;
 
-
-    /* Calculate camera pose */
-    // Attitude of the quad. How the body is rotated away from ENU by a 3-2-1 euler transform
-    // Azimuth measures angle away from true north. pi/2 - az shifts it into ENU
-    // A positive pitch is actually a negative rotation about the y axis
-    // Ref: https://en.wikipedia.org/wiki/Azimuth#True_north-based_azimuths
-    // quadAtt is roll(x), pitch(y), yaw(z)
-    const Eigen::Vector3d quadAtt(0, -gpsSolution.el, M_PI/2-gpsSolution.az);
-    const Eigen::Vector3d rpG(gpsSolution.x, gpsSolution.y, gpsSolution.z);
-    const Eigen::Vector3d cameraPos = transformBodyToECEF(
-        rpG, 
-        Eigen::Vector3d(0,0,0), 
-        sensorParams.rcB,
-        quadAtt(0),
-        quadAtt(1),
-        quadAtt(2));
-
-    // Attitude of camera with respect to ENU.
-    // Same as quad att just with a 30 degree pitch down
-    // Add pi/6 since pitching 'down' is actually a positive roll around the y axis
-    const Eigen::Vector3d cameraAtt = quadAtt + Eigen::Vector3d(0, M_PI/6, 0);
-    const Eigen::Vector4d cameraQuat = composeECEFQuat(rpG, cameraAtt(0), cameraAtt(1), cameraAtt(2));
-
     /* Write camera pose to file */
-    std::string data = "" + 
-        std::to_string(cameraPos(0)) + " " + 
-        std::to_string(cameraPos(1)) + " " + 
-        std::to_string(cameraPos(2)) + " " + 
-        std::to_string(cameraQuat(0)) + " " + 
-        std::to_string(cameraQuat(1)) + " " + 
-        std::to_string(cameraQuat(2)) + " " + 
-        std::to_string(cameraQuat(3));
+    std::string data = "";
+    {   // Scoping block
+        std::string command = "echo '# IMAGENAME X Y Z POSCOV EL AZ ELSIGMA AZSIGMA ATTCOV' >> " + saveDirectory + "/image_data_raw.txt";
+        std::system(command.c_str());
+    }
+    
+    // Write camera position
+    data = data + " " + std::to_string(gpsSolution.x);
+    data = data + " " + std::to_string(gpsSolution.y);
+    data = data + " " + std::to_string(gpsSolution.z);
 
-    std::string command = "echo '" + filename + " " + data + "' >> " + saveDirectory + "/image_poses.txt";
+    for(int i=0; i < 6; i++) {
+        data = data + " " + std::to_string(gpsSolution.posCov[i]);
+    }
+
+    // Write camera orientation
+    data = data + " " + std::to_string(gpsSolution.el);
+    data = data + " " + std::to_string(gpsSolution.az);
+
+    data = data + " " + std::to_string(gpsSolution.elSigma);
+    data = data + " " + std::to_string(gpsSolution.azSigma);
+ 
+    for(int i=0; i < 6; i++) {
+        data = data + " " + std::to_string(gpsSolution.attCov[i]);
+    }
+
+    std::string command = "echo '" + filename + data + "' >> " + saveDirectory + "/image_data_raw.txt";
     std::system(command.c_str());
     
     /* Saving image */
